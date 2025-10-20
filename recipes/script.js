7973d526309fbbce96690f10619164889cb100e0
juswamacbook <joshuaranin2@gmail.com>
2025-10-20 17:53:00 -0400

    added new stuff

--- recipes/script.js
+++
@@ -1,6 +0,0 @@
-<script type = "module">
-    import {Toast} from 'bootstrap.esm.js';
-
-    Array.from(document.querySelectorAll('.toast'))
-    .forEach(toastNode => new Toast(toastNode))
-</script>
